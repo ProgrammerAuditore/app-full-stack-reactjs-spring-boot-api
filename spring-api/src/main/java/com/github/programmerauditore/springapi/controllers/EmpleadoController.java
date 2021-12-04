27298de5692ad15eac05b501f93ca971c038ed4b
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 17:47:45 -0600

    *** Se creo una ruta en el controlador de empleado en Spring Boot Api

--- spring-api/src/main/java/com/github/programmerauditore/springapi/controllers/EmpleadoController.java
+++ spring-api/src/main/java/com/github/programmerauditore/springapi/controllers/EmpleadoController.java
@@ -1,10 +1,27 @@
 package com.github.programmerauditore.springapi.controllers;
 
+import com.github.programmerauditore.springapi.repositories.EmpleadoRepository;
+
+import java.util.List;
+
+import com.github.programmerauditore.springapi.models.EmpleadoModel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmpleadoController {
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    // obtener todo los empleados
+    @GetMapping( path="/empleados" )
+    public List<EmpleadoModel> getAllEmpleados(){
+        return empleadoRepository.findAll();
+    }
 
 }
--- spring-api/src/main/java/com/github/programmerauditore/springapi/repositories/EmpleadoRepository.java
+++ spring-api/src/main/java/com/github/programmerauditore/springapi/repositories/EmpleadoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmpledoRepository extends JpaRepository<EmpleadoModel, Long> {
+public interface EmpleadoRepository extends JpaRepository<EmpleadoModel, Long> {
 
 }
