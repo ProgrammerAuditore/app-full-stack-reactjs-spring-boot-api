cb927d056f190f8ea23e3f543e8af5811fec7ac4
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 17:34:25 -0600

    *** Se creo el repositorio Empleado en Spring Boot Api

---
+++ spring-api/src/main/java/com/github/programmerauditore/springapi/repositories/EmpledoRepository.java
@@ -0,0 +1,11 @@
+package com.github.programmerauditore.springapi.repositories;
+
+import com.github.programmerauditore.springapi.models.EmpleadoModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpledoRepository extends JpaRepository<EmpleadoModel, Long> {
+
+}
