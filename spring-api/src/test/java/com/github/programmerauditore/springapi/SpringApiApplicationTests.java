581816762b936bb5a3e818e7d580613fb2d32458
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 16:59:53 -0600

    *** Agregando el proyecto de Spring Boot

---
+++ spring-api/src/test/java/com/github/programmerauditore/springapi/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.programmerauditore.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
