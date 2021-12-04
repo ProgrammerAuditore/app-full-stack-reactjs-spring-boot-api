961da317204d8ba875574d29a61d64a3f0a75d8b
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 17:19:14 -0600

    *** Se creo el modelo para empleado en Spring Boot Api

---
+++ spring-api/src/main/java/com/github/programmerauditore/springapi/models/EmpleadoModel.java
@@ -0,0 +1,48 @@
+package com.github.programmerauditore.springapi.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "empleado")
+public class EmpleadoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String nombre;
+    private String puesto;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPuesto() {
+        return puesto;
+    }
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
