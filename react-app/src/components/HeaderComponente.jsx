6f6d94424c4ae05c012c194fd89b3cee9d21af33
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 21:28:55 -0600

    *** Se modifico el navbar del proyecto React JS
    Se usaron los componentes del modulo de bootstrap-react

--- react-app/src/components/HeaderComponente.jsx
+++ react-app/src/components/HeaderComponente.jsx
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Nav, Container, Navbar, NavDropdown } from "react-bootstrap";
 
 class HeaderComponente extends Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                          <div className="container">
-                            <a className="navbar-brand" href="#">Navbar</a>
-                            <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="collapsibleNavId">
-                                <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Home <span className="visually-hidden">(current)</span></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                        <div className="dropdown-menu" aria-labelledby="dropdownId">
-                                            <a className="dropdown-item" href="#">Action 1</a>
-                                            <a className="dropdown-item" href="#">Action 2</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                                <form className="d-flex my-2 my-lg-0">
-                                    <input className="form-control me-sm-2" type="text" placeholder="Search"/>
-                                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                                </form>
-                            </div>
-                      </div>
-                    </nav>
-                    
-                </header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                </Nav>
+                <Nav>
+                    <NavDropdown title="Empleados" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="/empleados">
+                        Listar
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="/crear-empleado">
+                        Crear
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                        Reportar
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default HeaderComponente;
