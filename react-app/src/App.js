313cdd1c60e9478f40d4b23bed3a60a1ac47f774
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 19:22:19 -0600

    *** Se instalo axios para en el proyecto React JS
    npm install axios

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,10 @@
+import ListarEmpleados from "./components/ListarEmpleados";
+
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo, react JS</h1>
+      <ListarEmpleados></ListarEmpleados>
     </div>
   );
 }
