fbde364456966d668b8b491889dc3f3c3a9c5552
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 20:53:03 -0600

    *** Se creo una nueva ruta para crear a un empleado

--- spring-api/src/main/java/com/github/programmerauditore/springapi/controllers/EmpleadoController.java
+++ spring-api/src/main/java/com/github/programmerauditore/springapi/controllers/EmpleadoController.java
@@ -9,6 +9,8 @@ import com.github.programmerauditore.springapi.models.EmpleadoModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +23,14 @@ public class EmpleadoController {
     private EmpleadoRepository empleadoRepository;
 
     // obtener todo los empleados
-    @GetMapping( path="/empleados" )
+    @GetMapping( path = "/empleados" )
     public List<EmpleadoModel> getAllEmpleados(){
         return empleadoRepository.findAll();
     }
 
+    // crear un empleado
+    @PostMapping( path = "/empleados")
+    public EmpleadoModel createEmpleado(@RequestBody EmpleadoModel empleado){
+        return empleadoRepository.save(empleado);
+    }
 }
