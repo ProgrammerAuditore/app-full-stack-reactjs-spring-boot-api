d28bfc67d902bfbe08f36b5d9b87d9ea90977198
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-05 17:18:03 -0600

    *** modifico el proyecto React JS
    Se modifico el titulo del proyecto

--- react-app/src/components/HeaderComponente.jsx
+++ react-app/src/components/HeaderComponente.jsx
@@ -8,7 +8,7 @@ class HeaderComponente extends Component {
         <header>
           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-              <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+              <Navbar.Brand href="/">Empleados-App</Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
