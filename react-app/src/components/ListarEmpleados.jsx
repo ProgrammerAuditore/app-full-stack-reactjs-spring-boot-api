5d5b8117a66dcff2ba39fad64792a8b451557ee6
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 18:41:01 -0600

    *** Se agrego contenido para el componente React JS para listar empleados

--- react-app/src/components/ListarEmpleados.jsx
+++ react-app/src/components/ListarEmpleados.jsx
@@ -6,13 +6,33 @@ class ListarEmpleados extends Component{
         super(props);
 
         this.state = {
-            
+            empleados : []
         }
     }
 
     render(){
         return (<div>
-            
+            <h2 className="text-center">Lista de empleados</h2>
+            <div className="row">
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Puesto</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>#1</td>
+                            <td>Carlos</td>
+                            <td>Desing</td>
+                            <td>carlos@example.com</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>);
     }
 }
