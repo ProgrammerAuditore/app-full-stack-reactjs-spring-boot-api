bbfda2a4e039926d6f78b21a12b3a53b83f32be1
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 20:44:07 -0600

    *** Se implemento react-router al proyecto de ReactJs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import ListarEmpleados from "./components/ListarEmpleados";
 import HeaderComponente from "./components/HeaderComponente";
 import FooterComponente from "./components/FooterComponente";
-import "./App.css";
 
 function App() {
   return (
     <div>
-      <HeaderComponente></HeaderComponente>
-      <div className="container">
-        <ListarEmpleados></ListarEmpleados>
-      </div>
-      <FooterComponente></FooterComponente>
+        <HeaderComponente></HeaderComponente>
+        <div className="container">
+          <Routes>
+            <Route path="/empleados" element={<ListarEmpleados/>}></Route>
+          </Routes>
+        </div>
+        <FooterComponente></FooterComponente>
     </div>
   );
 }
