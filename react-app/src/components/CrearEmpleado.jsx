b3801f4e57e48cdeb374608f1a2be9b4e828c2c9
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 22:13:43 -0600

    *** Se modifico el componente de CrearEmpleado en el proyecto React JS
    Se agrego la funcionalidad de capturar datos

--- react-app/src/components/CrearEmpleado.jsx
+++ react-app/src/components/CrearEmpleado.jsx
@@ -6,7 +6,11 @@ class CrearEmpleado extends Component {
     super(props);
 
     this.state = {
-      empleado: {},
+      empleado: {
+        nombre:'',
+        puesto: '',
+        email: ''
+      },
     };
 
     this.fncCrearEmpleado = this.fncCrearEmpleado.bind(this);
@@ -15,6 +19,20 @@ class CrearEmpleado extends Component {
   fncCrearEmpleado(event) {
     event.preventDefault();
     console.log("Creando un empleado...");
+  }
+
+  fncEntradaDatos = (event) => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      empleado:{
+        ...this.state.empleado,
+        [name] : value
+      }
+    });
+
+    console.log(event.target.name, event.target.value);
   }
 
   render() {
@@ -30,6 +48,9 @@ class CrearEmpleado extends Component {
                     <Form.Label>Nombre</Form.Label>
                     <Form.Control
                       type="text"
+                      name="nombre"
+                      value={this.state.empleado.nombre}
+                      onChange={this.fncEntradaDatos}
                       placeholder="Escribe el nombre"
                     />
                   </Form.Group>
@@ -38,13 +59,21 @@ class CrearEmpleado extends Component {
                     <Form.Label>Puesto</Form.Label>
                     <Form.Control
                       type="text"
+                      name="puesto"
+                      value={this.state.empleado.puesto}
+                      onChange={this.fncEntradaDatos}
                       placeholder="Escribe el puesto"
                     />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Escribe el email" />
+                    <Form.Control 
+                    type="email" 
+                    name="email"
+                    value={this.state.empleado.email}
+                    onChange={this.fncEntradaDatos}
+                    placeholder="Escribe el email" />
                   </Form.Group>
 
                   <Button variant="primary" type="submit">
