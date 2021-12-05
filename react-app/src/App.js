1a17e7fd328d821185bcfef5ccc0ed45e60149c9
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-04 20:07:58 -0600

    *** Se agrego el componente Header y Footer de para React Js proyecto..

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,15 @@
 import ListarEmpleados from "./components/ListarEmpleados";
+import HeaderComponente from "./components/HeaderComponente";
+import FooterComponente from "./components/FooterComponente";
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <h1>Hola mundo, react JS</h1>
+      <HeaderComponente></HeaderComponente>
       <ListarEmpleados></ListarEmpleados>
+      <FooterComponente></FooterComponente>
     </div>
   );
 }
---
+++ react-app/src/components/FooterComponente.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FooterComponente extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="footer">
+                    <span className="text-muted">Todos los derechos reservados 2021 | </span> 
+                    <a href="https://github.com/ProgrammerAuditore" target="_blank" rel="noopener noreferrer">ProgrammerAuditore</a>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default FooterComponente;
