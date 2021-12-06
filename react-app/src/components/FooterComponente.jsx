033f274610a7b2df9dac725c0ef5989bc06ffa5d
ProgrammerAuditore <victorvj098@gmail.com>
2021-12-05 18:01:25 -0600

    *** modifico el proyecto React JS
    Se modifico el componente FooterComponente

--- react-app/src/components/FooterComponente.jsx
+++ react-app/src/components/FooterComponente.jsx
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class FooterComponente extends Component {
-    render() {
-        return (
-            <div>
-                <footer className="footer">
-                    <span className="text-muted">Todos los derechos reservados 2021 | </span> 
-                    <a href="https://github.com/ProgrammerAuditore" target="_blank" rel="noopener noreferrer">ProgrammerAuditore</a>
-                </footer>
+  render() {
+    return (
+      <div>
+        <footer className="text-center footer-style">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 footer-col"></div>
+              <div className="col-md-4 footer-col">
+                <h3>Acerca de</h3>
+                <ul className="list-inline">
+                  <li>Todo los derechos reservados 2021</li>
+                  <li>
+                    <a
+                      target="_blank"
+                      href="https://github.com/ProgrammerAuditore"
+                      className="btn-social btn-outline"
+                      rel="noreferrer"
+                    >
+                      <i className="fa fa-github"></i> ProgrammerAuditore
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-4 footer-col"></div>
             </div>
-        );
-    }
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default FooterComponente;
